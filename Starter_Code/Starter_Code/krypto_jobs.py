4dfff6526fc6c1f67d567a284d980659c8a28f54
unknown <you@example.com>
2023-10-22 00:53:56 -0400

    update 4

--- Starter_Code/Starter_Code/krypto_jobs.py
+++ Starter_Code/Starter_Code/krypto_jobs.py
@@ -80,7 +80,7 @@ w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 # @TODO:
 # From `crypto_wallet.py import the functions generate_account, get_balance,
 #  and send_transaction
- from crypto_wallet import generate_account, get_balance, send_transaction
+from crypto_wallet import generate_account, get_balance, send_transaction
 
 ################################################################################
 # KryptoJobs2Go Candidate Information
@@ -156,12 +156,13 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-account = generate_account(w3)
+account = generate_account
 
 ##########################################
 
 # Write the client's Ethereum account address to the sidebar
-st.sidebar.write(account.address)
+# st.sidebar.write(account.address)
+st.sidebar.write("Ethereum Account Address")
 
 ##########################################
 # Step 1 - Part 5:
@@ -265,7 +266,7 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Calculate total `wage` for the candidate by multiplying the candidate’s hourly
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
-wage = (`candidate_database[person][3]`) * (`hours`)
+wage = (candidate_database[person][3]) * (hours)
 
 # @TODO
 # Write the `wage` calculation to the Streamlit sidebar
@@ -296,7 +297,7 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    transaction_hash = send_transaction(account: Any, candidate_address: Any, wage= Any)
+    transaction_hash = send_transaction(account, candidate_address, wage)
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
