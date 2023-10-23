fb3c6661b44eda1368f502cdbacf45179549ac58
unknown <you@example.com>
2023-10-22 23:27:16 -0400

    Update 5

--- Starter_Code/Starter_Code/krypto_jobs.py
+++ Starter_Code/Starter_Code/krypto_jobs.py
@@ -173,7 +173,7 @@ st.sidebar.write("Ethereum Account Address")
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-ether_balance = get_balance(w3, account.address)
+ether_balance = get_balance(Ethereum Account Address)
 st.sidebar.write(ether_balance)
 
 
